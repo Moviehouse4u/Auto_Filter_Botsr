6aeebc94c40137b806fa1fe84c822537d45d11b1
SR Developer <113035774+Moviehouse4u@users.noreply.github.com>
2026-03-30 19:57:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+2cC72kNxGTJhOTZl') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+VVq0kMx_64s0MmI1') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/shiftcmd') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/SRKDFLIX') # Update channel link for the bot
 
